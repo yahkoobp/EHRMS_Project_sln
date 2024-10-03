1f3bc3f16cd859dd6d2ae7ce93e61cc69d12b793
yahkoobp <yahkoobp007@gmail.com>
2024-10-03 23:57:16 +0530

    New med added

---
+++ EHRMS_Project/Pages/MedicationManager/NewMedication.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EHRMS_Project.Pages.MedicationManager
+{
+    /// <summary>
+    /// Interaction logic for NewMedication.xaml
+    /// </summary>
+    public partial class NewMedication : Window
+    {
+        public NewMedication()
+        {
+            InitializeComponent();
+        }
+    }
+}
